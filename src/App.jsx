08411504c425c6df5f2a4e0185041a5b611421e4
Sahil Deore <sahildeore1234@gmail.com>
2025-08-09 01:56:07 +0530

    update sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar.jsx';
 import HeroSection from './components/HeroSection.jsx';
-
+import FeatureSection from './components/FeatureSection.jsx';
 const App = () => {
   return (
     <>
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection />
       </div>
     </>
   );
